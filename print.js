3a74a436c559fd45afa4ca84ce8c7820b0f136f9
Ahire Tersoo <105167338+tersoo-ahire@users.noreply.github.com>
2022-06-21 01:36:48 -0700

    Add files via upload

---
+++ print.js
@@ -0,0 +1,11 @@
+function function_name(){
+    document.getElementById('name').innerText = "Ahire Tersoo David";
+}
+
+function function_height(){
+    document.getElementById('height').innerText = "6ft or 183cm";
+}
+
+function function_country(){
+    document.getElementById('country').innerText = "Nigeria by birth, Rwandan at Heart";
+}
